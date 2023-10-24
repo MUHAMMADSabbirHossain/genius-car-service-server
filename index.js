740828c6385f74e8ad0449c5de445c8a14c8895b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-24 12:06:44 +0600

    added hero end point

--- index.js
+++ index.js
@@ -123,6 +123,10 @@ app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
 
+app.get('/hero', (req, res)=> {
+    res.send('Hero meets hero ku');
+})
+
 app.listen(port, () => {
     console.log('Listening to port 5000');
 })
